9c8fad1959e6e8a32703b79879568bde9b6c5938
michael mariea <mariea.michael@gmail.com>
2019-12-20 09:07:23 -0700

    12/20/19 start

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,6 +4,7 @@ import { StockDetailsComponent } from './stock-details/app.stockdetails'
 import { Error404Component } from './errors/app.404component'
 import { StockRouteActivator } from './stock-details/stock-details.activator.service'
 import{ AboutPage } from './about/app.about-component'
+
 export const appRoutes:Routes = [
     { path: 'stocks', component: StockListComponent},
     { path: 'stocks/:id', component: StockDetailsComponent,
